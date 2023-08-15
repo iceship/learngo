e986e267aeb58662324b2ceeddddf4dc34b6eebb
Juil Kim <iceship@gmail.com>
2023-08-15 22:03:32 +0900

    HitURL

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.youtube.com",
+		"https://www.amazon.com",
+		"https://www.wikipedia.org",
+		"https://www.reddit.com",
+		"https://www.yahoo.com",
+		"https://www.twitter.com",
+		"https://www.instagram.com",
+		"https://www.linkedin.com",
+		"https://www.ebay.com",
+		"https://www.apple.com",
+		"https://www.bing.com",
+		"https://www.microsoft.com",
+		"https://www.pinterest.com",
+		"https://www.netflix.com",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
