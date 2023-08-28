4cfc7c02df56fbfe0e2c9cf839b926e33bf8a581
Iceship <iceship@gmail.com>
2023-08-28 10:48:57 +0900

    Hiturl with Go

--- main.go
+++ main.go
@@ -8,35 +8,43 @@ import (
 
 var errRequestFailed = errors.New("Request failed")
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
+	var results = make(map[string]string)
+	c := make(chan requestResult)
+
 	urls := []string{
-		"https://www.google.com",
-		"https://www.facebook.com",
-		"https://www.youtube.com",
-		"https://www.amazon.com",
-		"https://www.wikipedia.org",
-		"https://www.reddit.com",
-		"https://www.yahoo.com",
-		"https://www.twitter.com",
-		"https://www.instagram.com",
-		"https://www.linkedin.com",
-		"https://www.ebay.com",
-		"https://www.apple.com",
-		"https://www.bing.com",
-		"https://www.microsoft.com",
-		"https://www.pinterest.com",
-		"https://www.netflix.com",
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
 	for _, url := range urls {
-		hitURL(url)
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+		fmt.Println(result)
 	}
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- requestResult) {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
